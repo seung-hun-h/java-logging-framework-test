cd49aad8f13461634df0de6f6811461f1b8bafdb
seung-hun-h <dau4723@gmail.com>
2022-04-17 20:21:47 +0900

    init

---
+++ src/main/java/com/example/javaloggingframeworktest/JavaLoggingFrameworkTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaloggingframeworktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLoggingFrameworkTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLoggingFrameworkTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaloggingframeworktest/JavaLoggingFrameworkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaloggingframeworktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLoggingFrameworkTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
